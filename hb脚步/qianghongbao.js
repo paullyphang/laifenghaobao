298d226d82ac8957aa8f92d2eaec7fc835303195
paullyphang <paullyphang@foxmail.com>
2015-11-07 00:52:23 +0800

    hb脚步

---
+++ hb脚步/qianghongbao.js
@@ -0,0 +1,26 @@
+
+//抢红包
+var ts = 0.1;
+var JQuery=$;
+
+var	timer1,timer2;
+ 
+function findHb() {
+	if(JQuery(".middle_star")!=null) {
+		JQuery(".middle_star").click();
+	}else{
+		console.info("当前页面没有待拆红包");
+	}
+}
+
+timer2 = window.setInterval(findHb,ts*1000);
+
+function getHb(){
+	if(JQuery(".big_bonus_active_btn")!=null){
+		JQuery(".big_bonus_active_btn").click();
+	}else{
+		console.info("当前页面没有红包");
+	}
+}
+
+var	timer1 = window.setInterval(getHb,ts*1000);
